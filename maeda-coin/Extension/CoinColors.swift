a97595f92f13458ff1d40d5ee72bb2fdca268379
前田隆之 <yukinocist@maedatakayukinoMacBook-puro.local>
2020-05-30 14:52:43 +0900

    入力チェックとちゅうまで

---
+++ maeda-coin/Extension/CoinColors.swift
@@ -0,0 +1,24 @@
+//
+//  CoinColors.swift
+//  maeda-coin
+//
+//  Created by 前田隆之 on 2020/05/29.
+//  Copyright © 2020 himawari. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    
+    class func White() -> UIColor {
+        return UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    }
+
+    class func Pink1() -> UIColor {
+        return UIColor(red: 255/255, green: 243/255, blue: 247/255, alpha: 1)
+    }
+    
+    class func Pink2() -> UIColor {
+        return UIColor(red: 253/255, green: 165/255, blue: 166/255, alpha: 1)
+    }
+}
--- maeda-coin/Modules/RegisterUserViewController.swift
+++ maeda-coin/Modules/RegisterUserViewController.swift
@@ -32,19 +32,74 @@ class RegisterUserViewController: UIViewController {
         registerButton.layer.shadowColor = UIColor.black.cgColor
         registerButton.layer.shadowOpacity = 0.4
     }
-    
-    @IBAction func pushRegister(_ sender: Any) {
-    }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        /* ユーザーIDの入力チェック */
+        guard let userId = self.userId.text, !userId.isEmpty, 8 <= userId.count, userId.count <= 20 else {
+            let alert: UIAlertController = UIAlertController(title: "入力エラー", message: "ユーザーIDが不正です。もう一度ご入力ください。", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            self.userId.backgroundColor = UIColor.Pink2()
+            return
+        }
+        self.userId.backgroundColor = UIColor.White()
+        print(userId.count)
+        
+        /* ログインパスワードの入力チェック */
+        guard let loginPassword = self.loginPassword.text, !loginPassword.isEmpty, 8 <= loginPassword.count, loginPassword.count <= 20 else {
+            let alert: UIAlertController = UIAlertController(title: "入力エラー", message: "ログインパスワードが不正です。もう一度ご入力ください。", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            self.loginPassword.backgroundColor = UIColor.Pink2()
+            return
+        }
+        self.loginPassword.backgroundColor = UIColor.White()
+
+        /* ニックネームの入力チェック */
+        guard let nickname = self.nickname.text, !nickname.isEmpty, 1 <= nickname.count, nickname.count <= 20 else {
+            let alert: UIAlertController = UIAlertController(title: "入力エラー", message: "ニックネームが不正です。もう一度ご入力ください。", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            self.nickname.backgroundColor = UIColor.Pink2()
+            return
+        }
+        self.nickname.backgroundColor = UIColor.White()
+        print(nickname.count)
+        
+        /* メールアドレスの入力チェック */
+        guard let mailAddress = self.mailAddress.text, !mailAddress.isEmpty else {
+            let alert: UIAlertController = UIAlertController(title: "入力エラー", message: "メールアドレスが不正です。もう一度ご入力ください。", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            self.mailAddress.backgroundColor = UIColor.Pink2()
+            return
+        }
+        self.mailAddress.backgroundColor = UIColor.White()
+        
+        /* 電話番号の入力チェック */
+        guard let phoneNumber = self.phoneNumber.text, !phoneNumber.isEmpty else {
+            let alert: UIAlertController = UIAlertController(title: "入力エラー", message: "電話番号が不正です。もう一度ご入力ください。", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            self.phoneNumber.backgroundColor = UIColor.Pink2()
+            return
+        }
+        self.phoneNumber.backgroundColor = UIColor.White()
+        
         /* 遷移先のViewControllerを取得 */
         guard let nextView = segue.destination as? PhoneNumberAuthenicationViewController else { return }
+        
         /* 次画面に入力情報を受け渡し */
-        nextView.registerUserInformation.userId = self.userId.text
-        nextView.registerUserInformation.loginPassword = self.loginPassword.text
-        nextView.registerUserInformation.nickname = self.nickname.text
-        nextView.registerUserInformation.mailAddress = self.mailAddress.text
-        nextView.registerUserInformation.phoneNumber = self.phoneNumber.text
+        nextView.registerUserInformation.userId = userId
+        nextView.registerUserInformation.loginPassword = loginPassword
+        nextView.registerUserInformation.nickname = nickname
+        nextView.registerUserInformation.mailAddress = mailAddress
+        nextView.registerUserInformation.phoneNumber = phoneNumber
     }
     
     /* TextField以外の部分を押した時にキーボードを閉じる */
